bf415603e7cbd19a1d5e74a67f3bb02352821700
WhateverSir <69344029+WhateverSir@users.noreply.github.com>
2023-08-10 19:00:03 +0800

    Add files via upload

---
+++ 垃圾清理.bat
@@ -0,0 +1,19 @@
+@echo off 
+echo 小可爱正在扫垃圾，请吴偌灏主人稍等...... 
+del /f /s /q %systemdrive%\*.tmp 
+del /f /s /q %systemdrive%\*._mp 
+del /f /s /q %systemdrive%\*.log 
+del /f /s /q %systemdrive%\*.gid 
+del /f /s /q %systemdrive%\*.chk 
+del /f /s /q %systemdrive%\*.old 
+del /f /s /q %systemdrive%\recycled\*.* 
+del /f /s /q %windir%\*.bak 
+del /f /s /q %windir%\prefetch\*.* 
+rd /s /q %windir%\temp & md %windir%\temp 
+del /f /q %userprofile%\小甜饼s\*.* 
+del /f /q %userprofile%\recent\*.* 
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*" 
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*" 
+del /f /s /q "%userprofile%\recent\*.*" 
+echo 吴偌灏主人，扫完了，么么哒！好累（*_*`）哦!
+echo. & pause 
